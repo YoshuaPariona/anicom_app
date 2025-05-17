20eac0dc3cb9fe06cdffd0b9370fd7ee1446cadc
YoshuaPariona <loganpariona.1207@gmail.com>
2025-05-17 17:18:18 -0500

    Initial commit with Flutter project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# anicom_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/anicom_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.anicom_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
